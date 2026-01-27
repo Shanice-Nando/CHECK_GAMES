17de5664f8b520c0c16e53b6285af37cae71e369
Miguel Tsakem <migueltsakem@gmail.com>
2026-01-27 21:32:48 +0100

    ajout du fichier pour les joueurs

---
+++ Player.py
@@ -0,0 +1,16 @@
+from cartes import *
+
+class Player():
+    def __init__(self, namePlayer, listCardsPlayer):
+        self.namePlayer = namePlayer
+        self.listCardsPlayer = listCardsPlayer
+        self.listCardsPlayer = []
+
+    def playACard(self, index):
+        self.listCardsPlayer.pop(index)
+        print(f"le joueur a joué {self.listCardsPlayer[index].name_card()}")
+
+    def drawCards(self, number):
+        for i in range(0, number):
+            self.listCardsPlayer.append(Action.cards[0])
+            Action.cards.pop(0)
